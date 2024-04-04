1d9f529b114e54d82fa0de5d560ea9ca55fcb8fa
Trevor Higgins <higginst.th@gmail.com>
2024-04-04 07:14:07 -0700

    Update Script.js added userHandicaps

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ alert ('Welcome to Mosey Golf');
 let teeTime = "Golf";
 document.write (teeTime)
 
+let userHandicaps = [
+  {name:"tommy", handicap: 24},
+  {name:"kyle", handicap: 7},
+  {name:"jeff", handicap: 11},
+  {name:"trevor", handicap:22},
+  {name:"charlie", handicap :15}
+];
