8884f61aa4dd2aa59fe46a1f7bedd0534c43da79
Trevor Higgins <higginst.th@gmail.com>
2024-04-05 10:50:04 -0700

    updates scripts.js removed document.write
    removed all document.write functions to test

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,50 @@
 
 
-let golfers = [
-  { name: "tommy", handicap: 24, besCourse: "Silverbell" },
-  { name: "kyle", handicap: 7, bestCourse: "Randolph North" },
-  { name: "jeff", handicap: 11, bestCourse: "Del Urich" },
-  { name: "trevor", handicap: 22, bestCourse: "Fred Enke" },
-  { name: "charlie", handicap: 15, bestCourse: "El Rio" },
-  { name: "justin", handicap: 20, bestCourse: "Randolph North" }
-];
-
 //alert ('Welcome to Muni.');
-
-//Fred Enke
-let lowestscorefred = "Today's Lowest Score on Fred Enke: ";
-document.write(lowestscorefred)
-
-let fredscore = ''
-let fredpar = 72
-if (fredscore == false) {
-  document.write('No Scores Posted Today')
-}
-if (fredscore > fredpar) {
-  document.write("+", fredscore - fredpar)
-}
-else if ((fredscore < fredpar) && (fredscore !== ''))//this is a chained condiion when "fredscore" is not ' '
-{
-  document.write(fredscore - fredpar)
-}
-
-//Randolph North
-let lowestscorerandolph = "Today's Lowest Score on Randolph North: ";
-document.write(lowestscorerandolph)
-
-let randolphscore = ''
-let randolphpar = 72
-if (randolphscore == false) {
-  document.write('No Scores Posted Today')
-}
-if (randolphscore > randolphpar) {
-  document.write("+", randolphscore - randolphpar)
-}
-else if ((randolphscore < randolphpar) && (randolphscore !== '')) {
-  document.write(randolphscore - randolphpar)};
-
-  
+let golfers = [
+  { name: "Tommy", handicap: 24, besCourse: "Silverbell" },
+  { name: "Kyle", handicap: 7, bestCourse: "Randolph North" },
+  { name: "Jeff", handicap: 11, bestCourse: "Dell Urich" },
+  { name: "Trevor", handicap: 22, bestCourse: "Fred Enke" },
+  { name: "Charlie", handicap: 15, bestCourse: "El Rio" },
+  { name: "Justin", handicap: 20, bestCourse: "Randolph North" }
+];
 for (let i = 0; i<golfers.length; i++){
   document.write ((golfers[i].name) + " has a handicap of " + (golfers[i].handicap) + " and their best course is " + 
   (golfers[i].bestCourse))
 }
+
+
+//Fred Enke
+let lowestscorefred = "Today's Lowest Score on Fred Enke: ";
+(lowestscorefred)
+
+let fredscore = ''
+let fredpar = 72
+if (fredscore == false) {
+('No Scores Posted Today')
+}
+if (fredscore > fredpar) {
+("+", fredscore - fredpar)
+}
+else if ((fredscore < fredpar) && (fredscore !== ''))//this is a chained condiion when "fredscore" is not ' '
+{
+(fredscore - fredpar)
+}
+
+//Randolph North
+let lowestscorerandolph = "Today's Lowest Score on Randolph North: ";
+(lowestscorerandolph)
+
+let randolphscore = ''
+let randolphpar = 72
+if (randolphscore == false) {
+('No Scores Posted Today')
+}
+if (randolphscore > randolphpar) {
+("+", randolphscore - randolphpar)
+}
+else if ((randolphscore < randolphpar) && (randolphscore !== '')) {
+(randolphscore - randolphpar)};
+
+  
