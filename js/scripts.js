4f14651893b0522e45a7b1784d7bc8ca66238e46
Trevor Higgins <higginst.th@gmail.com>
2024-04-11 09:30:18 -0700

    created coursesRepo

--- js/scripts.js
+++ js/scripts.js
@@ -5,106 +5,172 @@
 //Need to define Handicap and Best Course Algorithm 
 
 //Course arrays : 
-let courseFredArray = [
-  { name: "Hole 1", Par: 4 },
-  { name: "Hole 2", Par: 5 },
-  { name: "Hole 3", Par: 3 },
-  { name: "Hole 4", Par: 4 },
-  { name: "Hole 5", Par: 4 },
-  { name: "Hole 6", Par: 3 },
-  { name: "Hole 7", Par: 4 },
-  { name: "Hole 8", Par: 4 },
-  { name: "Hole 9", Par: 5 },
-  { name: "Hole 10", Par: 4 },
-  { name: "Hole 11", Par: 3 },
-  { name: "Hole 12", Par: 5 },
-  { name: "Hole 13", Par: 4 },
-  { name: "Hole 14", Par: 5 },
-  { name: "Hole 15", Par: 3 },
-  { name: "Hole 16", Par: 4 },
-  { name: "Hole 17", Par: 4 },
-  { name: "Hole 18", Par: 4 },
+let coursesRepo = (function (){
+  let courseFredArray = [
+    { name: "Hole 1", Par: 4 },
+    { name: "Hole 2", Par: 5 },
+    { name: "Hole 3", Par: 3 },
+    { name: "Hole 4", Par: 4 },
+    { name: "Hole 5", Par: 4 },
+    { name: "Hole 6", Par: 3 },
+    { name: "Hole 7", Par: 4 },
+    { name: "Hole 8", Par: 4 },
+    { name: "Hole 9", Par: 5 },
+    { name: "Hole 10", Par: 4 },
+    { name: "Hole 11", Par: 3 },
+    { name: "Hole 12", Par: 5 },
+    { name: "Hole 13", Par: 4 },
+    { name: "Hole 14", Par: 5 },
+    { name: "Hole 15", Par: 3 },
+    { name: "Hole 16", Par: 4 },
+    { name: "Hole 17", Par: 4 },
+    { name: "Hole 18", Par: 4 },
+  ];
+  let courseElrioArray = [
+    { name: "Hole 1", Par: 4 },
+    { name: "Hole 2", Par: 4 },
+    { name: "Hole 3", Par: 4 },
+    { name: "Hole 4", Par: 3 },
+    { name: "Hole 5", Par: 4 },
+    { name: "Hole 6", Par: 3 },
+    { name: "Hole 7", Par: 4 },
+    { name: "Hole 8", Par: 4 },
+    { name: "Hole 9", Par: 5 },
+    { name: "Hole 10", Par: 4 },
+    { name: "Hole 11", Par: 4 },
+    { name: "Hole 12", Par: 4 },
+    { name: "Hole 13", Par: 3 },
+    { name: "Hole 14", Par: 4 },
+    { name: "Hole 15", Par: 4 },
+    { name: "Hole 16", Par: 4 },
+    { name: "Hole 17", Par: 3 },
+    { name: "Hole 18", Par: 5 },
+  ];
+  let courseRandolphArray = [
+    { name: "Hole 1", Par: 4 },
+    { name: "Hole 2", Par: 4 },
+    { name: "Hole 3", Par: 5 },
+    { name: "Hole 4", Par: 4 },
+    { name: "Hole 5", Par: 4 },
+    { name: "Hole 6", Par: 3 },
+    { name: "Hole 7", Par: 4 },
+    { name: "Hole 8", Par: 3 },
+    { name: "Hole 9", Par: 5 },
+    { name: "Hole 10", Par: 4 },
+    { name: "Hole 11", Par: 3 },
+    { name: "Hole 12", Par: 4 },
+    { name: "Hole 13", Par: 5 },
+    { name: "Hole 14", Par: 4 },
+    { name: "Hole 15", Par: 3 },
+    { name: "Hole 16", Par: 5 },
+    { name: "Hole 17", Par: 4 },
+    { name: "Hole 18", Par: 4 },
+  ];
+  let courseDelArray = [
+    { name: "Hole 1", Par: 4 },
+    { name: "Hole 2", Par: 3 },
+    { name: "Hole 3", Par: 5 },
+    { name: "Hole 4", Par: 4 },
+    { name: "Hole 5", Par: 3 },
+    { name: "Hole 6", Par: 5 },
+    { name: "Hole 7", Par: 4 },
+    { name: "Hole 8", Par: 4 },
+    { name: "Hole 9", Par: 3 },
+    { name: "Hole 10", Par: 4 },
+    { name: "Hole 11", Par: 3 },
+    { name: "Hole 12", Par: 4 },
+    { name: "Hole 13", Par: 5 },
+    { name: "Hole 14", Par: 4 },
+    { name: "Hole 15", Par: 4 },
+    { name: "Hole 16", Par: 4 },
+    { name: "Hole 17", Par: 3 },
+    { name: "Hole 18", Par: 4 },
+  ];
+  let courseSilverbellArray = [
+    { name: "Hole 1", Par: 4 },
+    { name: "Hole 2", Par: 5 },
+    { name: "Hole 3", Par: 3 },
+    { name: "Hole 4", Par: 4 },
+    { name: "Hole 5", Par: 4 },
+    { name: "Hole 6", Par: 3 },
+    { name: "Hole 7", Par: 5 },
+    { name: "Hole 8", Par: 4 },
+    { name: "Hole 9", Par: 3 },
+    { name: "Hole 10", Par: 4 },
+    { name: "Hole 11", Par: 4 },
+    { name: "Hole 12", Par: 3 },
+    { name: "Hole 13", Par: 4 },
+    { name: "Hole 14", Par: 4 },
+    { name: "Hole 15", Par: 4 },
+    { name: "Hole 16", Par: 3 },
+    { name: "Hole 17", Par: 4 },
+    { name: "Hole 18", Par: 5 },
+  ];
+
+let courses = [{name: "El Rio", holes:courseElrioArray}, 
+{name: "Randolph North", holes: courseRandolphArray}, 
+{name: "Fred Enke", holes: courseFredArray},
+{name: "Del Urich", holes: courseDelArray},
+{name: "Silverbell", holes: courseSilverbellArray}];
+
+
+function getCourses(){
+return courses;
+}
+function addCourse(newCourse){
+  courses.push(newCourse)
+}
+return {
+  addCourse: addCourse,
+  getCourses:getCourses
+}
+})();
+
+
+let maleGolfersRepo = (function (){
+  
+  let maleGolfers = [
+  { name: "Tommy", handicap: 24, bestCourse: "Silverbell" },
+  { name: "Kyle", handicap: 7, bestCourse: "Randolph North" },
+  { name: "Jeff", handicap: 11, bestCourse: "Dell Urich" },
+  { name: "Trevor", handicap: 22, bestCourse: "Fred Enke" },
+  { name: "Charlie", handicap: 15, bestCourse: "El Rio" },
+  { name: "Justin", handicap: 20, bestCourse: "Randolph North" }
+  ];
+function getMaleGolfers (){
+return maleGolfers;
+}
+function add(golfer) {
+maleGolfers.push (golfer);
+}
+return {
+  getMaleGolfers: getMaleGolfers,
+  add: add
+}
+
+})()
+
+let femaleGolfersRepo= (function() {
+
+let femaleGolfers = [
+  { name: "Sofia", handicap: 24, besCourse: "Silverbell" },
+  { name: "Rachel", handicap: 7, bestCourse: "Randolph North" },
+  { name: "Emily", handicap: 11, bestCourse: "Dell Urich" },
+  { name: "Tracy", handicap: 22, bestCourse: "Fred Enke" },
 ];
-let courseElrioArray = [
-  { name: "Hole 1", Par: 4 },
-  { name: "Hole 2", Par: 4 },
-  { name: "Hole 3", Par: 4 },
-  { name: "Hole 4", Par: 3 },
-  { name: "Hole 5", Par: 4 },
-  { name: "Hole 6", Par: 3 },
-  { name: "Hole 7", Par: 4 },
-  { name: "Hole 8", Par: 4 },
-  { name: "Hole 9", Par: 5 },
-  { name: "Hole 10", Par: 4 },
-  { name: "Hole 11", Par: 4 },
-  { name: "Hole 12", Par: 4 },
-  { name: "Hole 13", Par: 3 },
-  { name: "Hole 14", Par: 4 },
-  { name: "Hole 15", Par: 4 },
-  { name: "Hole 16", Par: 4 },
-  { name: "Hole 17", Par: 3 },
-  { name: "Hole 18", Par: 5 },
-];
-let courseRandolphArray = [
-  { name: "Hole 1", Par: 4 },
-  { name: "Hole 2", Par: 4 },
-  { name: "Hole 3", Par: 5 },
-  { name: "Hole 4", Par: 4 },
-  { name: "Hole 5", Par: 4 },
-  { name: "Hole 6", Par: 3 },
-  { name: "Hole 7", Par: 4 },
-  { name: "Hole 8", Par: 3 },
-  { name: "Hole 9", Par: 5 },
-  { name: "Hole 10", Par: 4 },
-  { name: "Hole 11", Par: 3 },
-  { name: "Hole 12", Par: 4 },
-  { name: "Hole 13", Par: 5 },
-  { name: "Hole 14", Par: 4 },
-  { name: "Hole 15", Par: 3 },
-  { name: "Hole 16", Par: 5 },
-  { name: "Hole 17", Par: 4 },
-  { name: "Hole 18", Par: 4 },
-];
-let courseDelArray = [
-  { name: "Hole 1", Par: 4 },
-  { name: "Hole 2", Par: 3 },
-  { name: "Hole 3", Par: 5 },
-  { name: "Hole 4", Par: 4 },
-  { name: "Hole 5", Par: 3 },
-  { name: "Hole 6", Par: 5 },
-  { name: "Hole 7", Par: 4 },
-  { name: "Hole 8", Par: 4 },
-  { name: "Hole 9", Par: 3 },
-  { name: "Hole 10", Par: 4 },
-  { name: "Hole 11", Par: 3 },
-  { name: "Hole 12", Par: 4 },
-  { name: "Hole 13", Par: 5 },
-  { name: "Hole 14", Par: 4 },
-  { name: "Hole 15", Par: 4 },
-  { name: "Hole 16", Par: 4 },
-  { name: "Hole 17", Par: 3 },
-  { name: "Hole 18", Par: 4 },
-];
-let courseSilverbellArray = [
-  { name: "Hole 1", Par: 4 },
-  { name: "Hole 2", Par: 5 },
-  { name: "Hole 3", Par: 3 },
-  { name: "Hole 4", Par: 4 },
-  { name: "Hole 5", Par: 4 },
-  { name: "Hole 6", Par: 3 },
-  { name: "Hole 7", Par: 5 },
-  { name: "Hole 8", Par: 4 },
-  { name: "Hole 9", Par: 3 },
-  { name: "Hole 10", Par: 4 },
-  { name: "Hole 11", Par: 4 },
-  { name: "Hole 12", Par: 3 },
-  { name: "Hole 13", Par: 4 },
-  { name: "Hole 14", Par: 4 },
-  { name: "Hole 15", Par: 4 },
-  { name: "Hole 16", Par: 3 },
-  { name: "Hole 17", Par: 4 },
-  { name: "Hole 18", Par: 5 },
-];
+ function getFemaleGolfers(){
+  return femaleGolfers;
+ }
+ function add (golfer) {
+  femaleGolfers.push (golfer);
+ }
+
+return {
+  add: add,
+  getFemaleGolfers: getFemaleGolfers
+} 
+})();
+
 
 //functions will need to be generated from these variable with a return of the highest score logged, respectively
 
@@ -168,50 +234,6 @@ function handicapPercentages(list) {
 
 
 
-let maleGolfersRepo = (function (){
-  
-  let maleGolfers = [
-  { name: "Tommy", handicap: 24, bestCourse: "Silverbell" },
-  { name: "Kyle", handicap: 7, bestCourse: "Randolph North" },
-  { name: "Jeff", handicap: 11, bestCourse: "Dell Urich" },
-  { name: "Trevor", handicap: 22, bestCourse: "Fred Enke" },
-  { name: "Charlie", handicap: 15, bestCourse: "El Rio" },
-  { name: "Justin", handicap: 20, bestCourse: "Randolph North" }
-  ];
-function getMaleGolfers (){
-return maleGolfers;
-}
-function add(golfer) {
-maleGolfers.push (golfer);
-}
-return {
-  getMaleGolfers: getMaleGolfers,
-  add: add
-}
-
-})()
-
-let femaleGolfersRepo= (function() {
-
-let femaleGolfers = [
-  { name: "Sofia", handicap: 24, besCourse: "Silverbell" },
-  { name: "Rachel", handicap: 7, bestCourse: "Randolph North" },
-  { name: "Emily", handicap: 11, bestCourse: "Dell Urich" },
-  { name: "Tracy", handicap: 22, bestCourse: "Fred Enke" },
-];
- function getFemaleGolfers(){
-  return femaleGolfers;
- }
- function add (golfer) {
-  femaleGolfers.push (golfer);
- }
-
-return {
-  add: add,
-  getFemaleGolfers: getFemaleGolfers
-} 
-})();
-
 (maleGolfersRepo.getMaleGolfers()).forEach (function(golfer) {
   console.log(golfer)
 });
@@ -221,3 +243,6 @@ return {
 (femaleGolfersRepo.getFemaleGolfers()).forEach(function(golfer){
   console.log (golfer)
 });
+
+
+
