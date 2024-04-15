04720a6f8eb9d0c5a34dff52f09136d2810973c3
Trevor Higgins <higginst.th@gmail.com>
2024-04-15 08:21:20 -0700

    removed "playground-script"

--- js/scripts.js
+++ js/scripts.js
@@ -143,51 +143,6 @@ coursesRepo.addListItem(course)
  });
 
 
-let maleGolfersRepo = (function (){
-  
-  let maleGolfers = [
-  { name: "Tommy", handicap: 24, bestCourse: "Silverbell" },
-  { name: "Kyle", handicap: 7, bestCourse: "Randolph North" },
-  { name: "Jeff", handicap: 11, bestCourse: "Dell Urich" },
-  { name: "Trevor", handicap: 22, bestCourse: "Fred Enke" },
-  { name: "Charlie", handicap: 15, bestCourse: "El Rio" },
-  { name: "Justin", handicap: 20, bestCourse: "Randolph North" }
-  ];
-function getMaleGolfers (){
-return maleGolfers;
-}
-function add(golfer) {
-maleGolfers.push (golfer);
-}
-return {
-  getMaleGolfers: getMaleGolfers,
-  add: add
-}
-
-})()
-
-let femaleGolfersRepo= (function() {
-
-let femaleGolfers = [
-  { name: "Sofia", handicap: 24, besCourse: "Silverbell" },
-  { name: "Rachel", handicap: 7, bestCourse: "Randolph North" },
-  { name: "Emily", handicap: 11, bestCourse: "Dell Urich" },
-  { name: "Tracy", handicap: 22, bestCourse: "Fred Enke" },
-];
- function getFemaleGolfers(){
-  return femaleGolfers;
- }
- function add (golfer) {
-  femaleGolfers.push (golfer);
- }
-
-return {
-  add: add,
-  getFemaleGolfers: getFemaleGolfers
-} 
-})();
-
-
 //functions will need to be generated from these variable with a return of the highest score logged, respectively
 
 let fredScore = 20
@@ -215,49 +170,32 @@ function lowestScore(par, course) {
   }
 }
 
-let fredLow = lowestScore(72, fredScore)
-let elrioLow = lowestScore(70, elrioScore)
-let delLow = lowestScore(70, delScore)
-let randolphLow = lowestScore(72, randolphScore)
-let silverbellLow = lowestScore(70, silverbellScore)
-/*console.log (fredLow)
-  console.log (randolphLow)
-  console.log (silverbellLow)
-  console.log (delLow)
-  console.log (elrioLow) */
-
-/* femaleGolfers.forEach(function (user) {
-console.log(user.name + ' has a handicap of ' + user.handicap);
-});  /*In this forEach() method the anonymous funtion is iterated over each item in the array there can only be one
-  parameter and in this case it is "user"*/ 
-
 
 function handicapPercentages(list) {
-  let messages = [];
+  let handicapList = [];
 
   list.forEach(function(player) {
     if (player.handicap < 8) {
-      messages.push(player.name + "'s in a group of 5% that break 80");
+      handicapList.push(player.name + "'s in a group of 5% that break 80");
     } else if (player.handicap >= 8 && player.handicap < 18) {
-      messages.push(player.name + "'s in a group of 25% that break 90");
+      handicapList.push(player.name + "'s in a group of 25% that break 90");
     } else if (player.handicap >= 18 && player.handicap < 28) {
-      messages.push(player.name + "'s in a group of 50% that break 100");
+      handicapList.push(player.name + "'s in a group of 50% that break 100");
     }
   });
 
-  return messages;
+  return handicapList;
 }
 
 
 
-(maleGolfersRepo.getMaleGolfers()).forEach (function(golfer) {
-  console.log(golfer)
+(maleGolfersRepo.getMaleGolfers()).forEach (function(golfer){
 });
 
 
 
 (femaleGolfersRepo.getFemaleGolfers()).forEach(function(golfer){
-  console.log (golfer)
+
 });
 
 
