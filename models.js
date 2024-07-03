a4de8006c7dae38a99b1f245b78c80e3788257cc
Trevor Higgins <higginst.th@gmail.com>
2024-07-03 12:47:17 -0700

    added a user schema with bcrypt hashing

---
+++ models.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose');
+const bcrypt = require ('bcrypt');
+
+let userSchema = mongoose.Schema(
+    {
+        Username: {type: String, required: true},
+        Password: {type: String, required: true},
+        Email: {type: String, required: true},
+        Birthay: Date
+});
+userSchema.statics.hashPassword = (password)=>{
+    return bcrypt.hashSync(password, 10);
+};
+userSchema.methods.validatePassword = function(password) {
+return bcrypt.compareSync(password, this.Password);
+};
+
+let User = mongoose.model('User', userSchema);
+
+module.exports.User = User;
