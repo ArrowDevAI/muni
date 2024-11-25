c02ccf97510550db6f5071189719cb82dc64ec49
Trevor Higgins <higginst.th@gmail.com>
2024-11-25 11:56:45 -0500

    adjusted user model passoword to allow for null entry for Oauth

--- models/userModel.js
+++ models/userModel.js
@@ -23,6 +23,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     password: {
       type: DataTypes.STRING(255),
+      validate: {
+      notEmpty : true,
+      },
+      allowNull : true
     },
     createdAt: {
       type: DataTypes.DATE,
