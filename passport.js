c46c4edbba4b3df785cc4c5a3e517a550901ce8e
Trevor Higgins <higginst.th@gmail.com>
2024-11-25 11:55:36 -0500

    adjust passport for sequelize, adjust user model passoword to allow for null entry for Oauth

--- passport.js
+++ passport.js
@@ -1,89 +1,95 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-//const Models = require('./models.js');
 const passportJWT = require('passport-jwt');
-
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
+const { Users } = require('./models/index');
 
-const { Users} = require('./models/index');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
-const JWTStrategy = passportJWT.Strategy;    
-    const ExtractJWT = passportJWT.ExtractJwt;
-
+// Local Strategy for username/password authentication
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: 'Username',
-            passwordField: 'Password'
-        },
-        async (username, password, callback) => {
-            await Users.findOne({ Username: username }) //Users here will need to be re-mapped to PostgreSQL
-                .then((user) => {
-                    if (!user) {
-                        console.log('incorrect username');
-                        return callback(null, false, {
-                            message: 'Incorrect username'
-                        });
-                    }
-                    if (!user.validatePassword(password)) {
-                        console.log('incorrect password');
-                        return callback(null, false, { message: 'Incorrect password.' });
-                    }
-                    console.log('finished');
-                    return callback(null, user);
-                })
-                .catch((error) => {
-                    if (error) {
-                        console.log(error);
-                        return callback(error);
-                    }
-                })
+  new LocalStrategy(
+    {
+      usernameField: 'username', // Match your field name in the model
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await Users.findOne({ where: { username } });
+        if (!user) {
+          console.log('Incorrect username');
+          return done(null, false, { message: 'Incorrect username.' });
         }
-    )
+
+        const isValidPassword = await user.validatePassword(password);
+        if (!isValidPassword) {
+          console.log('Incorrect password');
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        console.log('Authentication successful');
+        return done(null, user);
+      } catch (error) {
+        console.error('Error in LocalStrategy:', error);
+        return done(error);
+      }
+    }
+  )
 );
 
+// JWT Strategy for token-based authentication
 passport.use(
-    new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_SECRET 
-    }, async (jwtPayLoad, callback) => {
-        return await Users.findById(jwtPayLoad._id)
-            .then((user) => {
-                return callback(null, user);
-            })
-            .catch((error) => {
-                return callback(error)
-            });
-            
-    }));
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await Users.findByPk(jwtPayload.id); // Adjust based on your payload
+        if (!user) {
+          return done(null, false, { message: 'Token does not match any user.' });
+        }
+        return done(null, user);
+      } catch (error) {
+        console.error('Error in JWTStrategy:', error);
+        return done(error);
+      }
+    }
+  )
+);
 
-    passport.use(
-        new GoogleStrategy(
-            {
-                clientID: process.env.GOOGLE_CLIENT_ID,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "/auth/google/callback",
-                scope: [
-                    'https://www.googleapis.com/auth/userinfo.profile',
-                    'https://www.googleapis.com/auth/userinfo.email'
-                ]
-            },
-            async (accessToken, refreshToken, profile, done) => {
-                try {
-                    let user = await Users.findOne({ googleId: profile.id });
-                    if (!user) {
-                        user = await new Users({
-                            googleId: profile.id,
-                            name: profile.displayName,
-                            email: profile.emails[0].value
-                        }).save();
-                    }
-                    return done(null, user);
-                } catch (error) {
-                    return done(error);
-                }
-            }
-        )
-    );
-    module.exports = passport;
+// Google OAuth 2.0 Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+      scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+      ],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await Users.findOne({ where: { googleid: profile.id } });
+        if (!user) {
+          user = await Users.create({
+            googleid: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0]?.value,
+          });
+        }
+        return done(null, user);
+      } catch (error) {
+        console.error('Error in GoogleStrategy:', error);
+        return done(error);
+      }
+    }
+  )
+);
+
+module.exports = passport;
