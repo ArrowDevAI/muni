60bc57bb86426e3793e3ba0cfdffa795ce6dd0a0
Trevor Higgins <higginst.th@gmail.com>
2024-12-05 17:13:50 -0500

    edited callback URL in googleStrategy

--- passport.js
+++ passport.js
@@ -67,7 +67,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
       scope: [
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/userinfo.email',
