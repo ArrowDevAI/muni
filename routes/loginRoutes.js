ba9031932b7b5ac7be265ed901aa2f68c69bea68
Trevor Higgins <higginst.th@gmail.com>
2025-01-03 20:50:41 -0500

    updated login route to /login

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -15,7 +15,7 @@ let generateJWTToken = (user) => {
 }
 
 /* POST login. */
-app.post('/', (req, res) => {
+app.post('/login', (req, res) => {
   passport.authenticate('local', { session: false }, (error, user, info) => {
     if (error || !user) {
       return res.status(400).json({
